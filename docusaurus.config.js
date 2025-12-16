5bd2f2e84a467fff4a166b5e00923121c7882835
Simone Amico <simone.amico1103@gmail.com>
2025-12-16 20:20:43 +0100

    fix: branding title, favicon and i18n translations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'UX Engineer Log',
+  tagline: 'From Zero to UX Engineer',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
